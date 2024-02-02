00ec6fa4bafc6228e2fc82baacfef375d3db6e19
saifudinzezy <saifudin.zezy7@gmail.com>
2024-02-03 00:12:57 +0700

    sample

---
+++ upload_s3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="" enctype="multipart/form-data">
+        <div class="form-group">
+            <label><b>Select File:</b></label>
+            <input type="file" name="userfile" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" name="submit" value="Upload">
+        </div>
+    </form>
+</body>
+</html>
