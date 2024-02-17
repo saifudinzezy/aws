a3b1d16c69898d6658b6a13d0bc7c4cd71fe9a56
saifudinzezy <saifudin.zezy7@gmail.com>
2024-02-17 17:25:21 +0700

    update server

---
+++ composer.bash
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install php-cli unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer
---
+++ server.bash
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install httpd
+sudo apt-get install php php-mysql
